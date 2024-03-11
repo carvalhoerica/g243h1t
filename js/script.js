c249e2f6443db6d1be37239a305272a76eb49fe8
érica cristiana carvalho silva <110104684+carvalhoerica@users.noreply.github.com>
2024-03-11 12:30:10 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,3 +8,34 @@ fazerSorteio = () => {
 
     document.getElementById("resultado").innerHTML = sorteio;
 }
+
+function escolher_aba(btn){
+    for(let i = 1; i<= 4; i++){
+        let id_btn = "btn" +i;
+        let id_aba = "ab"+i;
+        document.getElementById(id_btn).style.backgroundColor = "#dbdbdb"
+        document.getElementById(id_btn).disabled = false;
+        document.getElementById(id_aba).style.display = "none";
+
+    }
+    if(btn == "1"){
+        document.getElementById("btn1").style.backgroundColor = "#a7c9e4"
+        document.getElementById("btn1").disabled = true;
+        document.getElementById("ab1").style.display = "none";
+    }
+    if(btn == "2"){
+        document.getElementById("btn2").style.backgroundColor = "#a7c9e4"
+        document.getElementById("btn2").disabled = true;
+        document.getElementById("ab2").style.display = "none";
+    }
+    if(btn == "3"){
+        document.getElementById("btn3").style.backgroundColor = "#a7c9e4"
+        document.getElementById("btn3").disabled = true;
+        document.getElementById("ab3").style.display = "none";
+    }
+    if(btn == "4"){
+        document.getElementById("btn4").style.backgroundColor = "#a7c9e4"
+        document.getElementById("btn4").disabled = true;
+        document.getElementById("ab4").style.display = "none";
+    }
+}
